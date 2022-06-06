949b00115699355efea926aa74c41ef71fd7f6fb
jandeller <35778941+jandeller@users.noreply.github.com>
2022-06-06 10:24:17 +0200

    Create TensorBoardUtils.py

---
+++ TensorBoardUtils.py
@@ -0,0 +1,39 @@
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import tensorflow as tf
+
+def plot_confusion_matrix(cm, class_names):
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Accent)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+    threshold = cm.max() / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    return figure
+
+
+def plot_to_image(figure):
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(figure)
+    buf.seek(0)
+
+    digit = tf.image.decode_png(buf.getvalue(), channels=4)
+    digit = tf.expand_dims(digit, 0)
+
+    return digit
